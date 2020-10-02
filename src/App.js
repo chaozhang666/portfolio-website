fbe0d69fa73c01ad808d3b2adfc7a51f6c80bae2
Chao Zhang <raidenchao@gmail.com>
2020-10-02 10:57:36 -0700

    keep using material ui

--- src/App.js
+++ src/App.js
@@ -54,81 +54,101 @@ function App() {
 
   return (
     <div className="App">
+      <ThemeProvider ThemeProvider theme={theme}>
 
-    <ThemeProvider ThemeProvider theme={theme}>
-      <Box className='header'>    
-        <Box className='container navigation'>
-            <Typography className='logo nav-item' variant='h4' onClick={handleLogo}>portfolio</Typography>
-            <Box className='nav-wrapper'>
-              <Typography
-                variant='h1'
-                onClick={handleNavigation}
-                className={isResearch ? 'active' : undefined}
-                className='nav-item'
+        {/* header  */}
+        <Box className='header'>    
+          <Container>
+            <Box
+              m={3} 
+              display='flex'
+              flexDirection='row'
+              justifyContent="space-between"
+              alignItems='center'
+            >
+              <Typography 
+                className='logo nav-item' 
+                variant='h4' 
+                onClick={handleLogo}
+                dispaly='inline'
               >
+                portfolio
+              </Typography>
+              <Box className='nav-wrapper'>
+                  <Typography
+                    variant='h1'
+                    onClick={handleNavigation}
+                    className='nav-item'
+                    className={isResearch ? 'active' : undefined}
+                  >
+                    Research
+                  </Typography>
+                  <Typography
+                    variant='h1'
+                    onClick={handleNavigation}
+                    className='nav-item'
+                    className={isProject ? 'active' : undefined}
+                  >
+                    Project
+                  </Typography>
+                  <Typography 
+                    variant='h1'
+                    onClick={handleNavigation}
+                    className='nav-item'
+                    className={isGame ? 'active' : undefined}
+                  >
+                    Game
+                  </Typography>
+                  <Typography
+                    variant='h1'
+                    onClick={handleNavigation}
+                    className='nav-item'
+                    className={isAbout  ? 'active' : undefined}
+                  >
+                    About
+                  </Typography>
+                </Box>
+            
+            </Box>
+          </Container>
+        </Box> 
+
+        {/* homepage  */}
+        <Box className='homepage'>
+          <img src='' />
+          <Typography variant='h3' paddingTop={2}>
+            Hi, I am Chao.
+          </Typography>
+        </Box>
+        
+        {/* research  */}
+        <Box className='section' id='research'>
+          <Container>
+            <Box p={5}>
+              <Typography variant='h4' style={{margin: '30px'}}>
                 Research
               </Typography>
-              <Typography
-                variant='h1'
-                className={isProject ? 'active' : undefined}
-                onClick={handleNavigation}
-                className='nav-item'
+              <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="center"
               >
-                Project
-              </Typography>
-              <Typography 
-                variant='h1'
-                className={isGame ? 'active' : undefined}
-                onClick={handleNavigation}
-                className='nav-item'
-              >
-                Game
-              </Typography>
-              <Typography
-                variant='h1'
-                className={isAbout  ? 'active' : undefined}
-                onClick={handleNavigation}
-                className='nav-item'
-              >
-                About
-              </Typography>
+                <Grid item xs={12} sm={6}>
+                  <h1 style={{border: '1px solid', margin: '20px 40px'}}>dddd</h1>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <h1>dddd</h1>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <h1>dddd</h1>
+                </Grid>
+              </Grid>
             </Box>
-          </Box>
-        </Box> 
-        </ThemeProvider>
+          </Container>
+        </Box>
 
-
-
-
-      <div className='homepage'>
-        <img src='' />
-        <h1>Hi, I am Chao.</h1>
-      </div>
-
-
-      <Box className='section1' id='research' >
-        <Container>
-          <h1>Research</h1>
-          <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item xs={12} sm={4}>
-              <h1 style={{border: '1px solid', margin: '20px 40px'}}>dddd</h1>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <h1>dddd</h1>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <h1>dddd</h1>
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
-
-
+      </ThemeProvider>
     </div>
   );
 }
