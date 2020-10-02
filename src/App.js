1ce81396e75478d8e679c494e837cfa2f7ed9c23
Chao Zhang <raidenchao@gmail.com>
2020-10-02 00:42:59 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [isResearch, setResearch] = useState(false);
+  const [isProject, setProject] = useState(false);
+  const [isGame, setGame] = useState(false);
+  const [isAbout, setAbout] = useState(false);
+
+  const resetNavigationState = () => {
+    setResearch(false);
+    setProject(false);
+    setGame(false);
+    setAbout(false);
+  }
+
+  const  handleNavigation = (e) => {
+    resetNavigationState();
+    switch(e.target.innerText) {
+      case 'Research': 
+        setResearch(true);
+        break;
+      case 'Project': 
+        setProject(true);
+        break;
+      case 'Game': 
+        setGame(true);
+        break;
+      case 'About': 
+        setAbout(true);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleLogo = () => {
+    resetNavigationState();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>    
+        <div className='container navigation'>
+            <h1 className='logo' onClick={handleLogo}>portfolio</h1>
+            <div className='nav-wrapper'>
+              <h1 
+                onClick={handleNavigation}
+                className={isResearch ? 'active' : undefined}
+              >
+                Research
+              </h1>
+              <h1 
+                className={isProject ? 'active' : undefined}
+                onClick={handleNavigation}
+              >
+                Project
+              </h1>
+              <h1 
+                className={isGame ? 'active' : undefined}
+                onClick={handleNavigation}
+              >
+                Game
+              </h1>
+              <h1 
+                className={isAbout  ? 'active' : undefined}
+                onClick={handleNavigation}
+              >
+                About
+              </h1>
+            </div>
+        </div>
+      </div> 
     </div>
   );
 }
