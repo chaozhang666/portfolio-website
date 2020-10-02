6b12dd75851b68e4dbe1c498df0b3f8882075638
Chao Zhang <raidenchao@gmail.com>
2020-10-02 02:12:54 -0700

    add material ui components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { useState } from 'react';
 import './App.css';
+import { useState } from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 function App() {
+
+  const theme = createMuiTheme({
+    typography: {
+      fontFamily: 'Cormorant Garamond'
+    }
+  });
 
   const [isResearch, setResearch] = useState(false);
   const [isProject, setProject] = useState(false);
@@ -42,37 +54,81 @@ function App() {
 
   return (
     <div className="App">
-      <div className='header'>    
-        <div className='container navigation'>
-            <h1 className='logo' onClick={handleLogo}>portfolio</h1>
-            <div className='nav-wrapper'>
-              <h1 
+
+    <ThemeProvider ThemeProvider theme={theme}>
+      <Box className='header'>    
+        <Box className='container navigation'>
+            <Typography className='logo nav-item' variant='h4' onClick={handleLogo}>portfolio</Typography>
+            <Box className='nav-wrapper'>
+              <Typography
+                variant='h1'
                 onClick={handleNavigation}
                 className={isResearch ? 'active' : undefined}
+                className='nav-item'
               >
                 Research
-              </h1>
-              <h1 
+              </Typography>
+              <Typography
+                variant='h1'
                 className={isProject ? 'active' : undefined}
                 onClick={handleNavigation}
+                className='nav-item'
               >
                 Project
-              </h1>
-              <h1 
+              </Typography>
+              <Typography 
+                variant='h1'
                 className={isGame ? 'active' : undefined}
                 onClick={handleNavigation}
+                className='nav-item'
               >
                 Game
-              </h1>
-              <h1 
+              </Typography>
+              <Typography
+                variant='h1'
                 className={isAbout  ? 'active' : undefined}
                 onClick={handleNavigation}
+                className='nav-item'
               >
                 About
-              </h1>
-            </div>
-        </div>
-      </div> 
+              </Typography>
+            </Box>
+          </Box>
+        </Box> 
+        </ThemeProvider>
+
+
+
+
+      <div className='homepage'>
+        <img src='' />
+        <h1>Hi, I am Chao.</h1>
+      </div>
+
+
+      <Box className='section1' id='research' >
+        <Container>
+          <h1>Research</h1>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            <Grid item xs={12} sm={4}>
+              <h1 style={{border: '1px solid', margin: '20px 40px'}}>dddd</h1>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <h1>dddd</h1>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <h1>dddd</h1>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+
+
     </div>
   );
 }
